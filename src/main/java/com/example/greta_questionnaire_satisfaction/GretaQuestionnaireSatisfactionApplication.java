4a0681e13835efad5f29743199f27c0d0ffb71be
JonOklm <madou222002@outlook.com>
2024-05-10 12:04:51 +0200

    first commit

---
+++ src/main/java/com/example/greta_questionnaire_satisfaction/GretaQuestionnaireSatisfactionApplication.java
@@ -0,0 +1,13 @@
+package com.example.greta_questionnaire_satisfaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GretaQuestionnaireSatisfactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GretaQuestionnaireSatisfactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/greta_questionnaire_satisfaction/GretaQuestionnaireSatisfactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greta_questionnaire_satisfaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GretaQuestionnaireSatisfactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
